c0c26efc3e79c62467f1c7cacf5746584d08fb24
subbni <slfkak@naver.com>
2025-06-06 21:35:56 +0900

    [feat] 투표 생성/조회 페이지 등록 (#3, #5) - 헤더도 추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,29 @@ import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
 import OAuth2RedirectHandler from './pages/OAuth2RedirectHandler';
 import { AuthProvider } from './contexts/AuthContext';
+import CreatePollPage from './pages/poll/CreatePollPage';
+import PollDetailPage from './pages/poll/PollDetailPage';
+import Header from './components/Header';
+import PollListPage from './pages/poll/PollListPage';
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/login' element={<LoginPage />} />
-          <Route path='/oauth2/redirect' element={<OAuth2RedirectHandler />} />
-        </Routes>
+        <Header /> {/* ✅ 모든 페이지에 고정 헤더 */}
+        <main className='pt-16'>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/login' element={<LoginPage />} />
+            <Route
+              path='/oauth2/redirect'
+              element={<OAuth2RedirectHandler />}
+            />
+            <Route path='/polls' element={<PollListPage />} />
+            <Route path='/polls/:pollId' element={<PollDetailPage />} />
+            <Route path='/poll/create' element={<CreatePollPage />} />
+          </Routes>
+        </main>
       </AuthProvider>
     </BrowserRouter>
   );
